253de110fa82b6b21f2b805be32751489cb1c49c
K-taiyi <korm.taiyi289@gmail.com>
2025-06-10 10:16:11 +0700

    first commit

---
+++ src/controller/CardController.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class CardController {
+    // use set collection : can't add duplicate product
+    // add sample 2-3 products to database
+    // user can add multi product at once to card
+    // add product by name and qty
+    // display all product in card
+    // search product in card
+    // display available products in database
+    // edit product like cancel product or change qty
+}
---
+++ src/controller/ProductController.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class ProductController {
+    private String name;
+    private Integer id;
+    private String uuid;
+    private Double price;
+    private boolean isDeleted;
+    private Integer qty;
+}
